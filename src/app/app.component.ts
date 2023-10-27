796ae4cdabc2aa4cf95cfdfe0e795eeebb042bc7
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-10-27 15:43:26 +0600

    video added for animation section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
 import { LangSwitcherModalComponent } from './modals/lang-switcher-modal/lang-switcher-modal.component';
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { UserLocalePreferenceModel } from './shared/models/user-local-preference.model';
 import { UserLocaleUtil } from './shared/services/user-locale-preference.service';
 import { ActivatedRoute } from '@angular/router';
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document,
     private modalService: BsModalService,
     private route: ActivatedRoute
   ) {
